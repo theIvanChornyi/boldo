174add4af42efdad113e22e025d6000cc8f8766a
Ivan Chornyi <theivanchornyi@gmail.com>
2024-03-20 19:33:09 +0200

    feat: Test deployment

--- src/modules/home.component.tsx
+++ src/modules/home.component.tsx
@@ -1,7 +1,9 @@
+import { Blog } from "./blog/blog.component"
 import { Hero } from "./hero/hero.component"
 
 export const Home: React.FC = () => {
     return <>
-        <Hero/>
+        <Hero />
+        <Blog/>
     </>
 }
